7dcad88b17cd20f68dfb767990b3585033fc1a90
RohanPunjani <rohanpunjani32@gmail.com>
2023-04-13 08:56:06 +0530

    Added small changes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.DB_URI)
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("Connected to the database"))
   .catch((err) => console.log("Something went Wrong!"));
 
